ff85e9dc7dfbd954c0bfb08ada932c1b528a23e1
gabrielgirardeli <gsilvacosta75@gmail.com>
2025-08-25 21:57:00 -0300

    feat: add entidade de usuarios

---
+++ backend/MeuCorre.core/Entities/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using MeuCorre.core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuCorre.core.Entities
+{
+   public class Categoria : Entidade
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public  TipoTransacao  tipo { get; set; }
+        public string COR { get; set; }
+        public string Icone { get; set; }
+        public Guid? UsuarioId { get; set; }
+        public DateTime DataCriacao { get;set; }
+        public bool Ativo { get;set; }
+    }
+
+    public Categoria (string nome, string descricao, TipoTransacao tipo, string cor, string)
+}
