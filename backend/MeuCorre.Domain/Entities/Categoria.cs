c2fcb5ba89182b8bca77aacb9847b2177fd79352
gabrielgirardeli <gsilva75@gmail.com>
2025-09-15 21:54:24 -0300

    feat(backend) #12  adcionar entidades categorias

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -13,10 +13,10 @@ namespace MeuCorre.Domain.Entities
       
 
         public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string? Descricao { get; set; }
         public TipoTransacao Tipo { get; set; }
-        public string Cor { get; set; }
-        public string Icone { get; set; }
+        public string? Cor { get; set; }
+        public string? Icone { get; set; }
         public bool Ativo { get;set; }
         public Guid? UsuarioId { get; set; }
         
@@ -25,15 +25,38 @@ namespace MeuCorre.Domain.Entities
 
         public virtual Usuario Usuario { get; set; }
 
-        public Categoria(string nome, string descricao, string cor, string icone, bool ativo, Guid usuarioid)
+        public Categoria(string nome, string? descricao, string? cor, string? icone, bool ativo, Guid usuarioid,TipoTransacao tipo)
         {
             Nome = nome;
             Descricao = descricao;
             Cor = cor;
             Icone = icone;
-            Ativo = ativo;
+            Ativo = true;
             UsuarioId = usuarioid;
+            Tipo = new TipoTransacao();
 
+        }
+
+        public void AtualizarInformacoes(string nome, string descricao, string cor, string icone, bool ativo, TipoTransacao tipo)
+        {
+            Nome = nome.ToUpper();
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            Ativo = true;
+            Tipo = new TipoTransacao();
+            AtualizarDataModificacao();
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataModificacao();
+        }
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataModificacao();
         }
 
     }
