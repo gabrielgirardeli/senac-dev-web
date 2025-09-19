c0940898685d80fd3e71752d3bfa77717e02185f
gabrielgirardeli <gsilva75@gmail.com>
2025-09-18 21:51:18 -0300

    feat(backend) criou o querry

--- backend/MeuCorre.Application/UseCases/Categorias/Commands/AtualiazarCategoriaCommand.cs
+++ backend/MeuCorre.Application/UseCases/Categorias/Commands/AtualiazarCategoriaCommand.cs
@@ -1,16 +1,11 @@
 ﻿using MediatR;
 using MeuCorre.Domain.Enums;
 using MeuCorre.Domain.Interfaces.Repositories;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MeuCorre.Application.UseCases.Categorias.Commands
 {
-    public class AtualiazarCategoriaCommand : IRequest<(string, bool)>
+    public class AtualizarCategoriaCommand : IRequest<(string, bool)>
     {
         [Required(ErrorMessage = "Id da categoria  é obrigatorio.")]
         public required Guid CategoriaId { get; set; }
@@ -24,14 +19,14 @@ namespace MeuCorre.Application.UseCases.Categorias.Commands
        
     }
     
-    internal class AtualizarCategoriaCommandHandler : IRequestHandler<AtualiazarCategoriaCommand, (string, bool)>
+    internal class AtualizarCategoriaCommandHandler : IRequestHandler<AtualizarCategoriaCommand, (string, bool)>
     {
         private readonly ICategoriaRepository _categoriaRepository;
         public AtualizarCategoriaCommandHandler(ICategoriaRepository categoriaRepository)
         {
             _categoriaRepository = categoriaRepository;
         }
-        public async Task<(string, bool)> Handle(AtualiazarCategoriaCommand request, CancellationToken cancellationToken)
+        public async Task<(string, bool)> Handle(AtualizarCategoriaCommand request, CancellationToken cancellationToken)
         {
             var categoria = await _categoriaRepository.ObterPorIdAsync(request.CategoriaId);
 
@@ -46,9 +41,10 @@ namespace MeuCorre.Application.UseCases.Categorias.Commands
                 return ("Já existe uma categoria com esse nome para o usuário.", false);
             }
 
-            await categoria.AtualizarInformacoes(request.Nome, request.Descricao, request.Cor, request.Icone, request.tipo);
+            categoria.AtualizarInformacoes(request.Nome, request.Descricao, request.Cor, request.Icone, request.tipo);
             await _categoriaRepository.AtualizarAsync(categoria);
             return ("Categoria atualizada com sucesso.", true);
+
 
         }
     }
